371a50fed3a279e8470f738638f9eb95e12675ec
David Benaben <david.benaben@u-bordeaux.fr>
2019-04-25 15:58:15 +0200

    first publication

---
+++ templates/users.auth.php
@@ -0,0 +1,13 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+
+root:$1$HoqccIT6$6z99PZMZi9n1UGW.838P20:Super User:root@ifb.local:admin,user
